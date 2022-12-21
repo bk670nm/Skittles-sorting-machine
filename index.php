8381e5bc32ace163b6766e030ae1e0ae942755c7
BarboraKubalcova <barborka.kubalcova@gmail.com>
2022-12-21 10:33:30 +0100

    sdasdasd

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
     <?php
         echo '<h1>Baska page</h1>';
         
-        $data = json_decode(file_get_contents('php://filter/resource/https://skittlessorting.azurewebsites.net/'), true);
+        $data = json_decode(file_get_contents('https://skittlessorting.azurewebsites.net/'), true);
         
         $r = $data["r"]
         $g = $data["g"]
@@ -17,3 +17,5 @@
         echo $text;
         echo "<div class='background-color: rgb($r,$g,$b);'></div>";
     ?>
+
+   
