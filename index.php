ec00f536828f4cf175acd0aa8b71b5fb6800c574
BarboraKubalcova <barborka.kubalcova@gmail.com>
2022-12-21 20:11:33 +0100

    sdf

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+  
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
     <?php
         echo '<h1>Baska page</h1>';
         
@@ -38,5 +51,13 @@
         echo $text3;
         fclose($file3);
     ?>
+
+    <section>
+        
+        <div>
+        </div>
+    </section>
+</body>
+</html>
 
    
