b820cded9c84823b9c809b1269c98406172c765e
BarboraKubalcova <barborka.kubalcova@gmail.com>
2022-12-21 10:42:15 +0100

    sadasd

--- index.php
+++ index.php
@@ -1,21 +1,42 @@
     <?php
         echo '<h1>Baska page</h1>';
         
-        $data = json_decode(file_get_contents('https://skittlessorting.azurewebsites.net/'), true);
+        //$data = json_decode(file_get_contents('https://skittlessorting.azurewebsites.net/'), true);
         
-        $r = $data["r"]
-        $g = $data["g"]
-        $b = $data["b"]
-        $t = $data["t"]
+        //$r = $data["r"]
+        //$g = $data["g"]
+        //$b = $data["b"]
+        //$t = $data["t"]
 
-        $db = fopen("db.txt", "w") or die("Unable to open file!");
-        fwrite($db, $data);
-        fclose($db);
+        //$db = fopen("db.txt", "w") or die("Unable to open file!");
+        //fwrite($db, $data);
+        //fclose($db);
         
-        $text = "r=" . $r . " g=" . $g . " b=" . $b . " t=" . $t;
+        //$text = "r=" . $r . " g=" . $g . " b=" . $b . " t=" . $t;
         // $sum = $sn1 + $sn2;
-        echo $text;
-        echo "<div class='background-color: rgb($r,$g,$b);'></div>";
+        //echo $text;
+        //echo "<div class='background-color: rgb($r,$g,$b);'></div>";
+
+        $r = $_GET["r"];
+        $g = $_GET["g"];
+        $b = $_GET["b"];
+        $t = $_GET["t"];
+        
+        $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
+        $text = "r=" . $r . " g=" . $g . " b=" . $b . " t=" . $t;
+        
+        fwrite($file1, $text1);
+        fclose($file1);
+        
+        $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
+        $text2 = "Value from actuator. Save this value to actuator.txt";
+        fwrite($file2, $text2);
+        fclose($file2);
+        
+        $file3 = fopen("actuator.txt","r") or die ("Subor neexistuje");
+        $text3 = fread($file3,filesize("actuator.txt"));
+        echo $text3;
+        fclose($file3);
     ?>
 
    
